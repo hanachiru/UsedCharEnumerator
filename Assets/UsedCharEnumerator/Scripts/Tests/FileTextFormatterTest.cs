db196d36f10e6bd29cea0b1142a77d60f929ac0c
hanachiru <tomoe.66.tomoe@gmail.com>
2021-05-21 13:33:06 +0900

    UsedCharEnumeratorWindow作成

--- Assets/UsedCharEnumerator/Scripts/Tests/FileTextFormatterTest.cs
+++ Assets/UsedCharEnumerator/Scripts/Tests/FileTextFormatterTest.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using NUnit.Framework;
 using UnityEngine;
-using UsedCharEnumerator;
 
-namespace Tests
+namespace UsedCharEnumerator.Tests
 {
     public class FileTextFormatterTest
     {
